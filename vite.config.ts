6794d9b0e918ae1b2e57f7e23aa292fe894d07a8
Kristian Gergov <kristian@gergov.bg>
2024-10-25 20:18:02 +0300

    Open the dev server to LAN

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: false,
+    host: "0.0.0.0",
   },
   test: {
     globals: true,
