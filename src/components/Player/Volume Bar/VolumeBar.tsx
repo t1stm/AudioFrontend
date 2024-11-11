906cf5b877b08f352e6c24d69e916496455c178b
Kristian Gergov <kristian@gergov.bg>
2024-11-11 16:06:34 +0200

    Support dragging on volume bar

--- src/components/Player/Volume Bar/VolumeBar.tsx
+++ src/components/Player/Volume Bar/VolumeBar.tsx
@@ -1,7 +1,12 @@
 import "./VolumeBar.scss"
 import type React from "react"
-import { useCallback } from "react"
-import { VolumeDownFill, VolumeMuteFill, VolumeOffFill, VolumeUpFill } from "react-bootstrap-icons"
+import { useCallback, useState, useEffect } from "react"
+import {
+  VolumeDownFill,
+  VolumeMuteFill,
+  VolumeOffFill,
+  VolumeUpFill,
+} from "react-bootstrap-icons"
 import { useAppDispatch, useAppSelector } from "../../../state/hooks"
 import type { RootState } from "../../../state/store"
 import { toggleMute } from "../../../state/player/playerSlice"
@@ -11,61 +16,100 @@ export interface VolumeBarProps {
   onChangeDelta: (delta: number) => void
 }
 
-const VolumeBar: React.FC<VolumeBarProps> = (props) => {
+const VolumeBar: React.FC<VolumeBarProps> = props => {
   const { onChange, onChangeDelta } = props
-  const { volume, muted } = useAppSelector((state: RootState) => {
-    return {
-      volume: state.player.volume,
-      muted: state.player.muted
-    }
-  })
+  const { volume, muted } = useAppSelector((state: RootState) => ({
+    volume: state.player.volume,
+    muted: state.player.muted,
+  }))
   const dispatch = useAppDispatch()
+  const [isDragging, setIsDragging] = useState(false)
 
   const onIconClick = () => {
     dispatch(toggleMute())
-  };
+  }
 
   const tempVolume = muted ? 0 : volume
 
   const volumeIcon =
-    tempVolume > .50 ?
-      <VolumeUpFill size={24} onClick={onIconClick}/> :
-    tempVolume > .16 ?
-      <VolumeDownFill size={24} onClick={onIconClick}/> :
-    tempVolume > 0 ?
-      <VolumeOffFill size={24} onClick={onIconClick}/> :
-      <VolumeMuteFill size={24} onClick={onIconClick}/>
+    tempVolume > 0.5 ? (
+      <VolumeUpFill size={24} onClick={onIconClick} />
+    ) : tempVolume > 0.16 ? (
+      <VolumeDownFill size={24} onClick={onIconClick} />
+    ) : tempVolume > 0 ? (
+      <VolumeOffFill size={24} onClick={onIconClick} />
+    ) : (
+      <VolumeMuteFill size={24} onClick={onIconClick} />
+    )
 
-  const handleClick = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
-
-    const rect = e.currentTarget.getBoundingClientRect()
-    const currentY = rect.bottom - e.clientY
+  const handleVolumeChange = useCallback((clientY: number, rect: DOMRect) => {
+    const currentY = rect.bottom - clientY
     const divHeight = rect.height
     const percentage = currentY / divHeight
     const clamped = Math.max(Math.min(percentage, 1), 0)
-
     onChange(clamped)
   }, [onChange])
 
-  const handleWheel = useCallback((delta: number) => {
-    const step = -delta / 1500;
-    onChangeDelta(step);
-  }, [onChangeDelta])
+  const handleMouseDown = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      const rect = e.currentTarget.getBoundingClientRect()
+      setIsDragging(true)
+      handleVolumeChange(e.clientY, rect)
+    },
+    [handleVolumeChange],
+  )
+
+  const handleMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (isDragging) {
+        const rect = document
+          .querySelector(".volume-bar")!
+          .getBoundingClientRect()
+        handleVolumeChange(e.clientY, rect)
+      }
+    },
+    [handleVolumeChange, isDragging],
+  )
+
+  const handleMouseUp = useCallback(() => {
+    setIsDragging(false)
+  }, [])
+
+  const handleWheel = useCallback(
+    (delta: number) => {
+      const step = -delta / 1500
+      onChangeDelta(step)
+    },
+    [onChangeDelta],
+  )
+
+  useEffect(() => {
+    if (isDragging) {
+      window.addEventListener("mousemove", handleMouseMove)
+      window.addEventListener("mouseup", handleMouseUp)
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+  }, [isDragging, handleMouseMove, handleMouseUp])
 
   const blipStyle = {
-    bottom: `${ tempVolume * 100 }%`
+    bottom: `${tempVolume * 100}%`,
   }
 
   return (
-    <div className="volume-box" onWheel={(e) => {
-      handleWheel(e.deltaY)
-    }}>
-      { volumeIcon }
-      <div className="volume-bar" onClick={handleClick}>
+    <div className="volume-box" onWheel={e => handleWheel(e.deltaY)}>
+      {volumeIcon}
+      <div className="volume-bar" onMouseDown={handleMouseDown}>
         <div className="volume-blip" style={blipStyle}></div>
       </div>
     </div>
   )
 }
 
-export default VolumeBar;
+export default VolumeBar
