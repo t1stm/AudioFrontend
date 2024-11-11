9db5122da4b7c3adfa24ba9e0038bef4faaeaf26
Kristian Gergov <kristian@gergov.bg>
2024-11-12 00:45:28 +0200

    Added the ability to set the bitrate and codecs

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -5,18 +5,20 @@ import type { ChangeEvent} from "react";
 import { useCallback } from "react"
 import type { RootState } from "../../state/store"
 import type { CodecInfo } from "../../objects/codecs"
-import { setCodec } from "../../state/settings/settingsSlice"
+import { setBitrate, setCodec } from "../../state/settings/settingsSlice"
 
+const BitRates = [8, 16, 32, 48, 64, 96, 112, 128, 160, 192, 224, 256, 320]
 const Settings = () => {
-  const { supportedCodecs } = useAppSelector((state: RootState) => {
+  const { currentCodec, supportedCodecs, bitrate } = useAppSelector((state: RootState) => {
     return {
       currentCodec: state.settings.currentCodec as CodecInfo,
-      supportedCodecs: state.settings.supportedCodecs as CodecInfo[]
+      supportedCodecs: state.settings.supportedCodecs as CodecInfo[],
+      bitrate: state.settings.bitrate
     }
   });
+
   const dispatch = useAppDispatch();
-  const selectCallback = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    console.log(e)
+  const codecSelect = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
     const selectedName = e.currentTarget.value;
 
     const codec = supportedCodecs.find(info => {
@@ -27,11 +29,23 @@ const Settings = () => {
     dispatch(setCodec(codec))
   }, [dispatch, supportedCodecs]);
 
+  const bitrateSelect = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.currentTarget.value;
+    const bitrate = Number.parseInt(value);
+    dispatch(setBitrate(bitrate))
+  }, [dispatch])
+
   return (
-    <div>
-      <select onChange={selectCallback}>
+    <div className={"settings"}>
+      <select onChange={codecSelect}>
         {supportedCodecs.map(c => (
-          <option key={c.name} value={c.name}>{c.name}</option>
+          <option key={c.name} value={c.name} selected={c.name === currentCodec.name}>{c.name}</option>
+        ))}
+      </select>
+
+      <select onChange={bitrateSelect}>
+        {BitRates.map(b => (
+          <option key={b} value={b} selected={bitrate === b}>{b}kbps</option>
         ))}
       </select>
     </div>
