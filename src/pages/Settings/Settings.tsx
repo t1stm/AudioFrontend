e6d62ff0e7155a2056c546481593eafa9267d4a5
Kristian Gergov <kristian@gergov.bg>
2024-11-12 02:02:09 +0200

    Start work on the settings page

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -9,45 +9,83 @@ import { setBitrate, setCodec } from "../../state/settings/settingsSlice"
 
 const BitRates = [8, 16, 32, 48, 64, 96, 112, 128, 160, 192, 224, 256, 320]
 const Settings = () => {
-  const { currentCodec, supportedCodecs, bitrate } = useAppSelector((state: RootState) => {
-    return {
-      currentCodec: state.settings.currentCodec as CodecInfo,
-      supportedCodecs: state.settings.supportedCodecs as CodecInfo[],
-      bitrate: state.settings.bitrate
-    }
-  });
+  const { currentCodec, supportedCodecs, bitrate } = useAppSelector(
+    (state: RootState) => {
+      return {
+        currentCodec: state.settings.currentCodec as CodecInfo,
+        supportedCodecs: state.settings.supportedCodecs as CodecInfo[],
+        bitrate: state.settings.bitrate,
+      }
+    },
+  )
 
-  const dispatch = useAppDispatch();
-  const codecSelect = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    const selectedName = e.currentTarget.value;
+  const dispatch = useAppDispatch()
+  const codecSelect = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      const selectedName = e.currentTarget.value
 
-    const codec = supportedCodecs.find(info => {
-      return info.name === selectedName;
-    })
+      const codec = supportedCodecs.find(info => {
+        return info.name === selectedName
+      })
 
-    if (!codec) return
-    dispatch(setCodec(codec))
-  }, [dispatch, supportedCodecs]);
+      if (!codec) return
+      dispatch(setCodec(codec))
+    },
+    [dispatch, supportedCodecs],
+  )
 
-  const bitrateSelect = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    const value = e.currentTarget.value;
-    const bitrate = Number.parseInt(value);
-    dispatch(setBitrate(bitrate))
-  }, [dispatch])
+  const bitrateSelect = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      const value = e.currentTarget.value
+      const bitrate = Number.parseInt(value)
+      dispatch(setBitrate(bitrate))
+    },
+    [dispatch],
+  )
 
   return (
     <div className={"settings"}>
-      <select onChange={codecSelect} value={currentCodec.name}>
-        {supportedCodecs.map(c => (
-          <option key={c.name} value={c.name}>{c.name}</option>
-        ))}
-      </select>
+      <p>Quality</p>
+      <span>These changes apply automatically for the next added song.</span>
+      <div className={"settings-section"}>
+        <span>Codec: </span>
+        <select onChange={codecSelect} value={currentCodec.name}>
+          {supportedCodecs.map(c => (
+            <option key={c.name} value={c.name}>
+              {c.name}
+            </option>
+          ))}
+        </select>
+      </div>
 
-      <select onChange={bitrateSelect} value={bitrate}>
-        {BitRates.map(b => (
-          <option key={b} value={b}>{b}kbps</option>
-        ))}
-      </select>
+      <div className={"settings-section"}>
+        <span>Bitrate: </span>
+        <select onChange={bitrateSelect} value={bitrate}>
+          {BitRates.map(b => (
+            <option key={b} value={b}>
+              {b}kbps
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <hr />
+      <p>Room Settings</p>
+      <span>These fields only work when connected to a room.</span>
+
+      <div className={"settings-section"}>
+        <span>Name: </span>
+        <input type={"text"} placeholder={"Name..."} />
+      </div>
+
+      <div className={"settings-section"}>
+        <span>Description: </span>
+        <input type={"text"} placeholder={"Description..."} />
+      </div>
+
+      <div className={"settings-section"}>
+        <button>Apply</button>
+      </div>
     </div>
   )
 }
