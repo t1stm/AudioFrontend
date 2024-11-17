daa797c0f7b36d90f54d02361f80b3b0707c4d7f
Kristian Gergov <kristian@gergov.bg>
2024-11-17 03:52:34 +0200

    Disable selecting the bitrate for FLAC

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,20 +1,24 @@
 import "./Settings.scss"
 
 import { useAppDispatch, useAppSelector } from "../../state/hooks"
-import type { ChangeEvent} from "react";
+import type { ChangeEvent } from "react";
+import { useState } from "react"
 import { useCallback } from "react"
 import type { RootState } from "../../state/store"
-import type { CodecInfo } from "../../objects/codecs"
 import { setBitrate, setCodec } from "../../state/settings/settingsSlice"
+import { updateRoomInfo } from "../../state/rooms/roomSlice"
+import playerService from "../../state/websockets/playerService"
 
 const BitRates = [8, 16, 32, 48, 64, 96, 112, 128, 160, 192, 224, 256, 320]
 const Settings = () => {
-  const { currentCodec, supportedCodecs, bitrate } = useAppSelector(
+  const { currentCodec, supportedCodecs, bitrate, initialRoomName, initialRoomDescription } = useAppSelector(
     (state: RootState) => {
       return {
-        currentCodec: state.settings.currentCodec as CodecInfo,
-        supportedCodecs: state.settings.supportedCodecs as CodecInfo[],
+        currentCodec: state.settings.currentCodec,
+        supportedCodecs: state.settings.supportedCodecs,
         bitrate: state.settings.bitrate,
+        initialRoomName: state.rooms.currentRoom?.name,
+        initialRoomDescription: state.rooms.currentRoom?.description,
       }
     },
   )
@@ -38,10 +42,16 @@ const Settings = () => {
     (e: ChangeEvent<HTMLSelectElement>) => {
       const value = e.currentTarget.value
       const bitrate = Number.parseInt(value)
+
+      if (!bitrate) return
       dispatch(setBitrate(bitrate))
     },
     [dispatch],
   )
+
+  const codecIsFLAC = currentCodec.name === "FLAC";
+  const [roomName, setRoomName] = useState(initialRoomName ?? "")
+  const [roomDescription, setRoomDescription] = useState(initialRoomDescription ?? "")
 
   return (
     <div className={"settings"}>
@@ -60,12 +70,19 @@ const Settings = () => {
 
       <div className={"settings-section"}>
         <span>Bitrate: </span>
-        <select onChange={bitrateSelect} value={bitrate}>
+        <select
+          onChange={bitrateSelect}
+          value={codecIsFLAC ? "Lossless" : bitrate}
+          disabled={codecIsFLAC}
+        >
           {BitRates.map(b => (
             <option key={b} value={b}>
               {b}kbps
             </option>
           ))}
+          {
+            codecIsFLAC ? <option value={"Lossless"}>Lossless VBR</option> : <></>
+          }
         </select>
       </div>
 
@@ -75,16 +92,44 @@ const Settings = () => {
 
       <div className={"settings-section"}>
         <span>Name: </span>
-        <input type={"text"} placeholder={"Name..."} />
+        <input
+          type={"text"}
+          placeholder={"Name..."}
+          value={roomName}
+          onChange={e => setRoomName(e.target.value)}
+        />
       </div>
 
       <div className={"settings-section"}>
         <span>Description: </span>
-        <input type={"text"} placeholder={"Description..."} />
+        <input
+          type={"text"}
+          placeholder={"Description..."}
+          value={roomDescription}
+          onChange={e => setRoomDescription(e.target.value)}
+        />
       </div>
 
       <div className={"settings-section"}>
-        <button>Apply</button>
+        <button
+          onClick={() => {
+            if (playerService.isConnected()) {
+              if (roomName.trim().length !== 0)
+                playerService.send(`updateroom name ${roomName}`)
+              if (roomDescription.trim().length !== 0)
+                playerService.send(`updateroom description ${roomDescription}`)
+              return
+            }
+            dispatch(
+              updateRoomInfo({
+                name: roomName,
+                description: roomDescription,
+              }),
+            )
+          }}
+        >
+          Apply
+        </button>
       </div>
     </div>
   )
